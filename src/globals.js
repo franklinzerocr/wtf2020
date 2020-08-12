eb510478906b6d0735e279cc71c9f9fa83deb140
Franklin Noriega <zerocreative.f@gmail.com>
2020-08-12 00:53:04 -0400

    remove globals

--- src/globals.js
+++
@@ -1,4 +0,0 @@
-export const backendURL = 'https://back.wtf2020.help';
-export const newsAPIURL = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/NewsSearchAPI?';
-export const xRapidapiHost = 'contextualwebsearch-websearch-v1.p.rapidapi.com';
-export const xRapidapiKey = 'f558f30306mshd50655455d86fc8p143db4jsn228e75d77415';
