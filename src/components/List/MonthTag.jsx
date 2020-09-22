87a1f936cc57440132c0ab2ac42bcfae1cd6071c
Franklin Noriega <zerocreative.f@gmail.com>
2020-09-21 20:12:01 -0400

    month tags fix

--- src/components/List/MonthTag.jsx
+++ src/components/List/MonthTag.jsx
@@ -9,19 +9,41 @@ export function setPositionOfMonthTags() {
 
     tag.style.height = height + 'px';
     tag.style.top = top + 'px';
+    tag.classList.remove('smallest-tag-size');
+    tag.classList.remove('small-tag-size');
+    tag.classList.remove('medium-tag-size');
     if (height <= 70) tag.classList.add('smallest-tag-size');
     else if (height <= 140) tag.classList.add('small-tag-size');
     else if (height <= 200) tag.classList.add('medium-tag-size');
   }
 }
-
+// UNIFINISHED FOR KEEP THE MONTH TAG ON DISPLAY
 export function monthTagAdjust() {
-  for (let tag of document.querySelectorAll('.month_tags .monthTag')) {
-    let topDiff = window.pageYOffset - cumulativeOffset(tag).top + 75;
-    if (topDiff > 10 && topDiff <= tag.offsetHeight - tag.querySelector('.monthTag-span').offsetHeight - 10) {
-      tag.querySelector('.monthTag-span').style.top = topDiff + 'px';
+  // let flag = null;
+  // let month_tags = document.querySelector('.month_tags');
+  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  if (width >= 768) {
+    for (let tag of document.querySelectorAll('.month_tags .monthTag')) {
+      let topDiff = window.pageYOffset - cumulativeOffset(tag).top + 75;
+      if (topDiff > 10 && topDiff <= tag.offsetHeight - tag.querySelector('.monthTag-span').offsetHeight - 10) {
+        tag.querySelector('.monthTag-span').style.top = topDiff + 'px';
+
+        // flag = true;
+        // let list_inner_container = month_tags.parentNode;
+
+        // let leftPos = cumulativeOffset(list_inner_container).left + list_inner_container.offsetWidth;
+        // console.log(leftPos);
+
+        // month_tags.classList.add('fixed');
+        // month_tags.style.left = leftPos + 'px';
+      }
     }
   }
+  // if (document.querySelectorAll('.month_tags .monthTag').length && !flag) {
+  //   document.querySelector('.month_tags').classList.remove('fixed');
+  //   month_tags.style.left = 'auto';
+  // }
 }
 
 function MonthTag() {
